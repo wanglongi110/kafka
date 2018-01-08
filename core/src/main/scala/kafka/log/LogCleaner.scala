2c20410dabcc635aa43592cc7b2db1cca6cf2fba
Navina Ramesh <nramesh@linkedin.com>
2018-01-08 13:44:30 -0800

    HOTFIX: Adding metrics for log compaction threads alive

--- core/src/main/scala/kafka/log/LogCleaner.scala
+++ core/src/main/scala/kafka/log/LogCleaner.scala
@@ -127,6 +127,13 @@ class LogCleaner(val config: CleanerConfig,
              def value: Int = cleaners.map(_.lastStats).map(_.elapsedSecs).max.toInt
            })
 
+  /* a metric to track the number of cleaner threads alive */
+  newGauge("live-cleaner-thread-count",
+          new Gauge[Int] {
+            def value: Int = cleaners.count(_.asInstanceOf[Thread].isAlive)
+          })
+
+
   /**
    * Start the background cleaning
    */
