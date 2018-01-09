2d55ba1a5f60bd99ceadfadfdf748d8d4b905370
Navina Ramesh <nramesh@linkedin.com>
2018-01-08 23:34:26 -0800

    Fixing record stats in LogCleaner. incorrect value was being passed as endOffset

--- core/src/main/scala/kafka/log/LogCleaner.scala
+++ core/src/main/scala/kafka/log/LogCleaner.scala
@@ -268,8 +268,8 @@ class LogCleaner(val config: CleanerConfig,
           var endOffset = cleanable.firstDirtyOffset
           try {
             val (nextDirtyOffset, cleanerStats) = cleaner.clean(cleanable)
-            recordStats(cleaner.id, cleanable.log.name, cleanable.firstDirtyOffset, endOffset, cleanerStats)
             endOffset = nextDirtyOffset
+            recordStats(cleaner.id, cleanable.log.name, cleanable.firstDirtyOffset, endOffset, cleanerStats)
           } catch {
             case _: LogCleaningAbortedException => // task can be aborted, let it go.
             case _: KafkaStorageException => // partition is already offline. let it go.
