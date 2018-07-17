2cf078af60bbc7d4d264b08f8336fbf07f6bd885
Jon Lee <jonlee@linkedin.com>
2018-07-17 12:58:44 -0700

    Log request quota metrics to kafka-quota-metrics.log
    RB=1362648
    BUG=LIKAFKA-18357
    G=Kafka-Code-Reviews
    R=dolin
    A=dolin

--- core/src/main/scala/kafka/server/ClientQuotaManager.scala
+++ core/src/main/scala/kafka/server/ClientQuotaManager.scala
@@ -192,8 +192,9 @@ class ClientQuotaManager(private val config: ClientQuotaManagerConfig,
     metricsMap.foreach {
       case (metricName: MetricName, kafkaMetric: KafkaMetric) =>
         if (metricName.group().equals(quotaType.toString) &&
-          (metricName.name().equals("byte-rate") || metricName.name().equals("throttle-time")) &&
-          metricName.tags().containsKey("client-id")) {
+          (metricName.name().equals("byte-rate") || metricName.name().equals("throttle-time") ||
+            metricName.name().equals("request-time")) &&
+          (metricName.tags().containsKey("client-id") || metricName.tags().containsKey("user"))) {
           logger.info("Metric name (" + metricName + ") has value (" + kafkaMetric.value() + ")")
         }
     }
