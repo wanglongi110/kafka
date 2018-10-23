bfd50aac5eea17fa9edca12acc96ec095011e215
Abhishek Mendhekar <amendhekar@linkedin.com>
2018-10-22 22:05:54 -0700

    [HOTFIX] LIKAFKA-19608 Refresh bootstrap server in ConsumerFetcherManager when TopicMetadata request fails
    As part of GDPR the bootstrap server were fetched from the ZK for fetching TopicMetadata. But this
    list is never refreshed. Recently all the broker in Kafka cluster were swapped out resulting
    in TopicMetadata fetch request to fail as the requests were sent to old broker and never refreshed
    after failed request.
    
    RB=1460191
    BUG=LIKAFKA-19608
    R=zahuang,amendhek
    A=zahuang

--- core/src/main/scala/kafka/consumer/ConsumerFetcherManager.scala
+++ core/src/main/scala/kafka/consumer/ConsumerFetcherManager.scala
@@ -23,13 +23,13 @@ import kafka.cluster.{BrokerEndPoint, Cluster}
 import org.apache.kafka.common.TopicPartition
 import org.apache.kafka.common.protocol.{ApiKeys, SecurityProtocol}
 import org.apache.kafka.common.{Cluster => JCluster}
+import org.apache.kafka.common.Node
 import org.apache.kafka.common.requests.{MetadataResponse => JMetadataResponse, MetadataRequest => JMetadataRequest}
 import scala.collection.immutable
 import collection.mutable.HashMap
 import scala.collection.mutable
 import java.util.concurrent.locks.ReentrantLock
 import scala.collection.JavaConversions._
-
 import kafka.utils.CoreUtils.inLock
 import kafka.utils.{ShutdownableThread, ThreadDeathListener, ZkUtils}
 import kafka.client.ClientUtils
@@ -56,14 +56,17 @@ class ConsumerFetcherManager(private val consumerIdString: String,
   private val correlationId = new AtomicInteger(0)
 
   private val isSSL = config.sslConfigs.isDefined
+  private val metadataUpdater = new ManualMetadataUpdater()
   private val metadataNetworkClientOpt = config.sslConfigs.map(sslConfigs => {
-    val bootstrapServers = seqAsJavaList(ClientUtils.getSslBrokerEndPoints(zkUtils).map(_.connectionString))
-    val addresses = org.apache.kafka.clients.ClientUtils.parseAndValidateAddresses(bootstrapServers)
-    val bootstrapNodes = JCluster.bootstrap(addresses).nodes
-    val metadataUpdater = new ManualMetadataUpdater()
-    metadataUpdater.setNodes(bootstrapNodes)
+    metadataUpdater.setNodes(bootstrapNodes())
     new SSLNetworkClient(config, metadataUpdater)
   })
+
+  private def bootstrapNodes() : util.List[Node] = {
+    val bootstrapServers = seqAsJavaList(ClientUtils.getSslBrokerEndPoints(zkUtils).map(_.connectionString))
+    val addresses = org.apache.kafka.clients.ClientUtils.parseAndValidateAddresses(bootstrapServers)
+    JCluster.bootstrap(addresses).nodes
+  }
 
   private class LeaderFinderThread(name: String) extends ShutdownableThread(name) {
     // thread responsible for adding the fetcher to the right broker when leader is available
@@ -124,8 +127,19 @@ class ConsumerFetcherManager(private val consumerIdString: String,
         case t: Throwable => {
             if (!isRunning.get())
               throw t /* If this thread is stopped, propagate this exception to kill the thread. */
-            else
+            else {
               warn("Failed to find leader for %s".format(noLeaderPartitionSet), t)
+              if (isSSL) {
+                try {
+                  metadataUpdater.setNodes(bootstrapNodes())
+                } catch {
+                  case t: Throwable => {
+                    warn("Unable to update bootstrap nodes", t)
+                    // Retry
+                  }
+                }
+              }
+            }
           }
       } finally {
         lock.unlock()
